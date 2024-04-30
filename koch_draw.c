6bc964ce227e7c4bd8e434049d95aa7f31bc1058
victtorm <victtor.moraes@gmail.com>
2024-04-30 15:37:29 +0100

    ok

---
+++ koch_draw.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   koch_draw.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbritto- <vbritto-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/26 20:12:53 by vbritto-          #+#    #+#             */
+/*   Updated: 2024/04/27 13:24:57 by vbritto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+void	draw_igual(t_fractal *koch, t_koch c, t_koch d)
+{
+	if (c.x > d.x)
+	{
+		while (c.x > d.x)
+		{
+			koch->c.r = c.x / koch->zoom + koch->r_move;
+			koch->c.i = c.y / koch->zoom + koch->i_move;
+			if (koch->c.i < HEIGHT && koch->c.i > 0
+				&& koch->c.r < WIDTH && koch->c.r > 0)
+				ft_put_pixels(koch, koch->c.r, koch->c.i, koch->color_1);
+			c.x--;
+		}
+	}
+	else
+	{
+		while (c.x < d.x)
+		{
+			koch->c.r = c.x / koch->zoom + koch->r_move;
+			koch->c.i = c.y / koch->zoom + koch->i_move;
+			if (koch->c.i < HEIGHT && koch->c.i > 0
+				&& koch->c.r < WIDTH && koch->c.r > 0)
+				ft_put_pixels(koch, koch->c.r, koch->c.i, koch->color_1);
+			c.x++;
+		}
+	}
+}
+
+void	draw_up(t_fractal *koch, t_koch c, t_koch d)
+{
+	while (c.x > d.x)
+	{
+		koch->c.r = c.x / koch->zoom + koch->r_move;
+		koch->c.i = c.y / koch->zoom + koch->i_move;
+		if (koch->c.i < HEIGHT && koch->c.i > 0
+			&& koch->c.r < WIDTH && koch->c.r > 0)
+			ft_put_pixels(koch, koch->c.r, koch->c.i, koch->color_1);
+		c.x -= 1 / 1.732;
+		c.y++;
+	}
+	while (c.x < d.x)
+	{
+		koch->c.r = c.x / koch->zoom + koch->r_move;
+		koch->c.i = c.y / koch->zoom + koch->i_move;
+		if (koch->c.i < HEIGHT && koch->c.i > 0
+			&& koch->c.r < WIDTH && koch->c.r > 0)
+			ft_put_pixels(koch, koch->c.r, koch->c.i, koch->color_1);
+		c.x += 1 / 1.732;
+		c.y++;
+	}
+}
+
+void	draw_down(t_fractal *koch, t_koch c, t_koch d)
+{
+	while (c.x > d.x)
+	{
+		koch->c.r = c.x / koch->zoom + koch->r_move;
+		koch->c.i = c.y / koch->zoom + koch->i_move;
+		if (koch->c.i < HEIGHT && koch->c.i > 0
+			&& koch->c.r < WIDTH && koch->c.r > 0)
+			ft_put_pixels(koch, koch->c.r, koch->c.i, koch->color_1);
+		c.x -= 1 / 1.732;
+		c.y--;
+	}
+	while (c.x < d.x)
+	{
+		koch->c.r = c.x / koch->zoom + koch->r_move;
+		koch->c.i = c.y / koch->zoom + koch->i_move;
+		if (koch->c.i < HEIGHT && koch->c.i > 0
+			&& koch->c.r < WIDTH && koch->c.r > 0)
+			ft_put_pixels(koch, koch->c.r, koch->c.i, koch->color_1);
+		c.x += 1 / 1.732;
+		c.y--;
+	}
+}
---
+++ koch_utils.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   koch_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbritto- <vbritto-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/24 15:52:50 by vbritto-          #+#    #+#             */
+/*   Updated: 2024/04/27 15:32:05 by vbritto-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fractol.h"
+
+int	check_koch(char *str1, char *str2)
+{
+	ft_strtolower(str1);
+	if (str1 == NULL || ft_strlen(str1) != 4)
+		return (1);
+	while ((*str1 == *str2) && (*str1 != '\0'))
+	{
+		str1++;
+		str2++;
+	}
+	return (*str1 - *str2);
+}
+
+void	start_koch(t_fractal *fractal)
+{
+	fractal->r_move = 0;
+	fractal->i_move = 0;
+	fractal->zoom = 1;
+	fractal->inter = 10;
+	fractal->coord[0].x = 50;
+	fractal->coord[0].y = 300;
+	fractal->coord[1].x = 650;
+	fractal->coord[1].y = 300;
+	fractal->color_1 = BLUE;
+	fractal->color_2 = RED;
+}
+
+void	init_koch(t_fractal *fractal)
+{
+	fractal->mlx_connection = mlx_init();
+	if (fractal->mlx_connection == NULL)
+		free(fractal->mlx_connection);
+	fractal->mlx_window = mlx_new_window(fractal->mlx_connection,
+			WIDTH, HEIGHT, fractal->set);
+	if (fractal->mlx_window == NULL)
+	{
+		mlx_destroy_display(fractal->mlx_connection);
+		free(fractal->mlx_connection);
+	}
+	fractal->img = mlx_new_image(fractal->mlx_connection,
+			WIDTH, HEIGHT);
+	if (fractal->img == NULL)
+	{
+		mlx_destroy_window(fractal->mlx_connection, fractal->mlx_window);
+		mlx_destroy_display(fractal->mlx_connection);
+		free(fractal->mlx_connection);
+	}
+	fractal->pixels_adr = mlx_get_data_addr(fractal->img,
+			&fractal->bits_per_pixel, &fractal->line_len, &fractal->endian);
+	start_koch(fractal);
+}
